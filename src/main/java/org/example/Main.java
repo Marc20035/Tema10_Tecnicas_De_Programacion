c11b171c3787eb76f2a5c7b1fbad5cf97bcfbde1
Marc <mlopesab@myuax.com>
2023-03-18 11:35:14 +0100

    Commit 1

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+import static org.example.RaizCuadrada.auxiliar;
+
+public class Main {
+    public static void main(String[] args) {
+        auxiliar();
+    }
+}
---
+++ src/main/java/org/example/RaizCuadrada.java
@@ -0,0 +1,25 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class RaizCuadrada {
+    public void raizCuadrada(double numero) {
+        if (numero < 0) {
+            System.out.println("No se puede sacar la raiz cuadrada de un numero negativo");
+            return;
+        }else {
+            double raiz = Math.sqrt(numero);
+            System.out.println("La raiz cuadrada de " + numero + " es " + raiz);
+        }
+    }
+    public void mostrarMenu() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduce un numero para sacar su raiz cuadrada: ");
+        double numero = sc.nextDouble();
+        raizCuadrada(numero);
+    }
+    public static void auxiliar() {
+        RaizCuadrada raiz = new RaizCuadrada();
+        raiz.mostrarMenu();
+    }
+}
