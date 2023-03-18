b37629da4a135144ae497d2b84c42a4c1d00d495
Marc <mlopesab@myuax.com>
2023-03-18 19:15:07 +0100

    Commit 2c

--- src/main/java/org/example/Ejercicio2/Main.java
+++ src/main/java/org/example/Ejercicio2/Main.java
@@ -1,5 +1,11 @@
 package org.example.Ejercicio2;
 
-public class Main {
+import java.util.Scanner;
 
+import static org.example.Ejercicio2.Operaciones.auxiliar;
+
+public class Main {
+    public static void main(String[] args) {
+        auxiliar();
+    }
 }
--- src/main/java/org/example/Ejercicio2/Operaciones.java
+++ src/main/java/org/example/Ejercicio2/Operaciones.java
@@ -1,6 +1,8 @@
 package org.example.Ejercicio2;
 
 
+import java.util.Scanner;
+
 public class Operaciones {
     private int capacidad;
 
@@ -66,5 +68,60 @@ public class Operaciones {
 
     public void mostrar(){
         System.out.println("El resultado es: " + getCapacidad());
+    }
+    public void mostrarMenuMain(){
+        Operaciones op = new Operaciones();
+        Scanner sc = new Scanner(System.in);
+        while(op.getCapacidad() > -2147483648 && op.getCapacidad() < 2147483647){
+            try {
+                System.out.println("Introduce un numero");
+                int num = sc.nextInt();
+                System.out.println("Introduce una operacion: +, -, *, /");
+                String operacion = sc.next();
+                switch (operacion) {
+                    case "+":
+                        try {
+                            op.suma(num);
+                        } catch (DesbordaCapacidadException e) {
+                            System.out.println(e.getMessage());
+                        }
+                        break;
+                    case "-":
+                        try {
+                            op.resta(num);
+                        } catch (DesbordaCapacidadException e) {
+                            System.out.println(e.getMessage());
+                        }
+
+                        break;
+                    case "*":
+                        try {
+                            op.multiplicacion(num);
+                        } catch (DesbordaCapacidadException e) {
+                            System.out.println(e.getMessage());
+                        }
+                        break;
+                    case "/":
+                        op.division(num);
+                        break;
+                    case "s":
+                        System.out.println("Saliendo...");
+                        System.exit(0);
+                        break;
+                    default:
+                        System.out.println("Operacion no valida");
+                        break;
+                }
+            } catch (Exception e) {
+                System.out.println("Introduce un numero valido");
+                sc.next();
+            }
+
+        }
+
+    }
+    public static void auxiliar(){
+        Operaciones op = new Operaciones();
+        op.mostrarMenuMain();
     }
 }
